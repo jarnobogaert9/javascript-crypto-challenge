747f16d2cb0cc67dbba0a41f669e5538dc2d0092
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-10-12 15:20:46 +0200

    Feat: encryptor module

---
+++ src/Encryptor.js
@@ -0,0 +1,16 @@
+const _sodium = require('libsodium-wrappers');
+module.exports = async (key) => {
+    await _sodium.ready;
+    const sodium = _sodium;
+
+    return Object.freeze({
+        encrypt(msg) {
+            const nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
+            const ciphertext =  sodium.crypto_secretbox_easy(msg, nonce, key);
+            return {
+                nonce: nonce,
+                ciphertext: ciphertext
+            }       
+        }
+    });
+};
